78dbc8cb4c9e52a3178a9fc8c21ccbd87b94e630
Kevin Winata <kevinwinatamichael@gmail.com>
2019-05-23 18:08:16 +0800

    add creds

--- creds.py
+++ creds.py
@@ -1,2 +1,33 @@
+import os
+import pickle
+
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+import settings
+
+
 class Creds:
-    pass
+    @staticmethod
+    def get_creds():
+        pickle_path = os.path.join(settings.ROOT_DIR, "token.pickle")
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists(pickle_path):
+            with open(pickle_path, 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    os.path.join(settings.ROOT_DIR, 'credentials.json'),
+                    ['https://www.googleapis.com/auth/spreadsheets'])
+                creds = flow.run_local_server()
+            # Save the credentials for the next run
+            with open(pickle_path, 'wb') as token:
+                pickle.dump(creds, token)
+        return creds
--- test/test_creds.py
+++ test/test_creds.py
@@ -1,8 +1,32 @@
+import os
+import time
 import unittest
+
+import settings
+from creds import Creds
 
 
 class CredsTestCases(unittest.TestCase):
-    pass
+    pickle_path = os.path.join(settings.ROOT_DIR, "token.pickle")
+
+    def test_get_creds_by_timing(self):
+        if os.path.exists(self.pickle_path):
+            os.remove(self.pickle_path)
+
+        start_time = time.time()
+        creds = Creds.get_creds()
+        end_time = time.time()
+
+        self.assertTrue((end_time- start_time) >= 1)
+        self.assertTrue(creds.valid)
+        self.assertEqual('Credentials', creds.__class__.__name__)
+
+        start_time = time.time()
+        creds = Creds.get_creds()
+        end_time = time.time()
+        self.assertFalse((end_time - start_time) >= 1)
+        self.assertTrue(creds.valid)
+        self.assertEqual('Credentials', creds.__class__.__name__)
 
 
 if __name__ == '__main__':
