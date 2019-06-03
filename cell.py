cbd1236f03543afa469dfa97accab1d1be022e4c
Kevin Winata <kevinwinatamichael@gmail.com>
2019-06-03 15:55:52 +0800

    Implement Cell data structures

--- cell.py
+++ cell.py
@@ -1,2 +1,7 @@
 class Cell:
-    pass
+    allowed_types = (int, str, float)
+
+    def __init__(self, value):
+        if not (isinstance(value, Cell.allowed_types)):
+            raise TypeError("Cell value must be in {}".format(Cell.allowed_types))
+        self.value = value
--- formatted_cell.py
+++ formatted_cell.py
@@ -1,2 +1,24 @@
-class FormattedCell:
-    pass
+from cell import Cell
+
+
+class FormattedCell(Cell):
+    def __init__(self, value, bgColor, bold):
+        super(FormattedCell, self).__init__(value)
+
+        if not isinstance(bgColor, dict):
+            raise TypeError("bgColor must be dictionary")
+
+        if not isinstance(bold, bool):
+            raise TypeError("bold must be boolean")
+
+        if {"red", "green", "blue"}.issubset(set(bgColor.keys())):
+            raise KeyError("bgColor must contain red, green, and blue keys")
+
+        for v in bgColor.values():
+            if not isinstance(v, (int, float)):
+                raise TypeError("bgColor value must be int or float")
+            if not 0 <= v <= 1:
+                raise ValueError("bgColor value must be between 0 and 1")
+
+        self.bgColor = bgColor
+        self.bold = bold
